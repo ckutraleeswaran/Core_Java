2ce17fc213e9db021010734ea0696f719aaaa905
ckutraleeswaran <kutraleeswaranc@gmail.com>
2026-05-07 12:17:19 +0530

    coindition statement with switch by kutraleeswaranC

---
+++ src/switchCase/org/Switch.java
@@ -0,0 +1,44 @@
+package switchCase.org;
+
+public class Switch {
+	
+	
+	
+	public void switchCondition() {
+	
+	int day = 5;
+	
+		switch(day) {
+
+		case 1:
+			System.out.println("Sunday");
+			break;
+		case 2:
+			System.out.println("Monday");
+			break;
+		case 3:
+			System.out.println("Tuesday");
+			break;
+		case 4:
+			System.out.println("Wednesday");
+			break;
+		case 5:
+			System.out.println("Thursday");
+			break;
+		case 6:
+			System.out.println("Friday");
+			break;
+		case 7:
+			System.out.println("Saturday");
+			break;
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Switch object = new Switch();
+		object.switchCondition();
+	}
+
+}
