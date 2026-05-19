25d5cce4918e546d8296d16c22f67cf8dd39edf0
KutraleeswaranC <Kutraleeswaranc@gmail.com>
2026-05-19 12:40:16 +0530

    nested loop

---
+++ src/loops/org/NestedLoop.java
@@ -0,0 +1,62 @@
+package loops.org;
+
+public class NestedLoop {
+	
+	
+//	Nested for loop
+//	—--------------------
+
+//	for( initialization ; condition ; increment / decrement) { 
+//
+//		for( initialization ; condition ; increment / decrement) { 
+
+//		}
+//	}
+
+	public void nestedForLoop() {
+		
+		for(int i = 1 ; i <= 3 ; i++ ) {
+			
+				System.out.println("i loop : " + i);
+			
+			for(int j = 1 ; j <= 3 ; j++ ) {
+				
+				System.out.println(" i + j = " + (i + j));	//addition)
+			}
+		}
+		
+		
+	}
+	public void rightHalfPyramid() {
+		//Expected output
+		
+//		1
+//		1 2
+//		1 2 3
+//		1 2 3 4
+//		1 2 3 4 5
+		
+		for(int i = 1 ; i <= 5 ; i++) {
+			
+			for(int j = 1 ; j <= i ; j++ ) {
+				
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		
+		NestedLoop obj = new NestedLoop();
+		
+//		System.out.print("my name sandy");
+//		System.out.println("my name sandy");
+		
+		obj.nestedForLoop();
+		obj.rightHalfPyramid();
+		
+	}
+
+}
